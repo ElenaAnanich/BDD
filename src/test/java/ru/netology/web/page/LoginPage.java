37dee1e1a3bb73dad8401ee6073a3ed15baef1a4
Lena k <ktyecz09@mail.ru>
2023-07-17 23:23:26 +0300

    add initial commit

--- src/test/java/ru/netology/web/page/LoginPage.java
+++ src/test/java/ru/netology/web/page/LoginPage.java
@@ -7,15 +7,15 @@ import ru.netology.web.data.DataHelper;
 import static com.codeborne.selenide.Selenide.page;
 
 public class LoginPage {
-    @FindBy(css ="[data-test-id=login] input")
+    @FindBy(css = "[data-test-id=login] input")
     private static SelenideElement loginField;
 
-    @FindBy (css ="[data-test-id=password] input")
+    @FindBy(css = "[data-test-id=password] input")
     private static SelenideElement passwordField;
-    @FindBy(css ="[data-test-id=action-login]")
+    @FindBy(css = "[data-test-id=action-login]")
     private static SelenideElement loginButton;
 
-    public VerificationPage validLogin(DataHelper.AuthInfo info){
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
         loginField.setValue(info.getLogin());
         passwordField.setValue(info.getPassword());
         loginButton.click();
